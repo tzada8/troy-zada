73e532329c4de3467f4d4958e8f04dbe8dd2bb1b
tzada8 <tzada@uwaterloo.ca>
2021-03-11 22:46:30 -0500

    added basic card frame for other pages

--- src/components/cards/experience/Experience.js
+++ src/components/cards/experience/Experience.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Experience.css";
+import "../Cards.css";
 
 function Experience() {
    return (
-      <div>
-         <h1>EXPERIENCE</h1>
+      <div className="card-box">
+         <div className="card-container">
+            {/* TITLE OF CARD */}
+            <div className="card-title">
+               <h1>EXPERIENCE</h1>
+            </div>
+
+            {/* REST OF CONTENT */}
+            <div className="card-content">
+               <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Totam aliquam inventore ipsam vel eveniet reprehenderit,
+                  voluptates excepturi tempora, accusantium hic, sed quod
+                  distinctio sequi? Eveniet dicta incidunt voluptatum quis quasi
+                  voluptate at quidem dolor natus architecto sed sequi ut
+                  corporis iste minus maxime delectus, mollitia cum neque
+                  consectetur ad! Rem cumque praesentium libero reiciendis animi
+                  culpa. Cupiditate similique, perferendis, doloribus commodi
+                  optio fugiat suscipit tempora, quisquam officiis nihil
+                  corrupti! Libero perspiciatis earum totam pariatur,
+                  voluptatibus architecto odit molestiae labore qui hic cum
+                  culpa odio expedita a sint consequuntur est, fuga accusamus
+                  ducimus dolore laborum numquam. Sit incidunt blanditiis quod
+                  ea?
+               </p>
+            </div>
+         </div>
       </div>
    );
 }
