76acf7c5ff61d7f08514ee64f8141dfcb0c6cf17
tzada8 <tzada@uwaterloo.ca>
2021-03-11 20:43:27 -0500

    created card frame/added common styles to App.css

--- src/components/cards/home/Home.js
+++ src/components/cards/home/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Home.css";
+import "../Cards.css";
 
 function Home() {
    return (
-      <div className="">
-         <h1>HOME</h1>
+      <div className="card-box">
+         <div className="card-container">
+            {/* TITLE OF CARD */}
+            <div className="card-title">
+               <h1>HOME</h1>
+            </div>
+
+            {/* REST OF CONTENT */}
+            <div className="card-content">
+               <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quaerat nobis quasi eius tempora possimus optio unde
+                  reprehenderit quis ab sunt!
+               </p>
+            </div>
+         </div>
       </div>
    );
 }
