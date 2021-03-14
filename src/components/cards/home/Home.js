cbb8f65f006b176e3e945363f69cd7b2b2e99c94
tzada8 <tzada@uwaterloo.ca>
2021-03-13 22:11:19 -0500

    created custom scrollbar

--- src/components/cards/home/Home.js
+++ src/components/cards/home/Home.js
@@ -13,20 +13,21 @@ function Home() {
    return (
       <div className="card-box home-page">
          <div className="card-container">
+            {/* ROUND HEADSHOT IMAGE */}
             <div className="home-img-round" />
 
             {/* TITLE OF CARD */}
             <h1 className="home-intro-name">Hi, I'm Troy Zada!</h1>
             {/* CURRENT ROLE/POSITION */}
-            <h2>Management Engineering Student @ University of Waterloo</h2>
+            <h2>Programmer, Lorem, ipsum.</h2>
 
             <hr className="home-page-separator" />
 
             {/* BRIEF OVERALL DESCRIPTION */}
             <p className="card-paragraphs">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-               incidunt, cumque aliquid deleniti optio velit at odit aspernatur
-               unde ea!
+               A passionate, hardworking, and dedicated Management Engineering
+               Student at the University of Waterloo. Lorem ipsum dolor sit amet
+               consectetur, adipisicing elit. Eaque, eius.
             </p>
 
             {/* SOCIAL ICON LINKS */}
