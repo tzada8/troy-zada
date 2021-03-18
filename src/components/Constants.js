b61b1c190b7d85ca7441e26483c27473a6cade83
tzada8 <tzada@uwaterloo.ca>
2021-03-18 17:02:56 -0400

    cleaned up code for homepage

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -6,13 +6,11 @@ export const EXPERIENCE = "EXPERIENCE";
 export const BLOG = "BLOG";
 export const CONTACT = "CONTACT";
 
-// SOCIAL MEDIA LINKS
+// SOCIAL MEDIA LINKS / CONTACT
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/troy-zada";
 export const FACEBOOK_LINK = "https://www.facebook.com/Troy.M.Zada";
-
-// CONTACTS
 export const EMAIL = "tzada@uwaterloo.ca";
-export const PHONE = "647-526-0838";
 
 export const FIRST_NAME = "Troy";
 export const LAST_NAME = "Zada";
+export const FULL_NAME = FIRST_NAME + " " + LAST_NAME;
--- src/components/cards/home/Home.js
+++ src/components/cards/home/Home.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./Home.css";
 import "../Cards.css";
 import {
-   PHONE,
    EMAIL,
    FACEBOOK_LINK,
    LINKEDIN_LINK,
-   FIRST_NAME,
-   LAST_NAME,
+   FULL_NAME,
 } from "../../Constants";
 
 function Home() {
@@ -20,9 +18,7 @@ function Home() {
             <div className="home-intro-title">
                <h2 className="home-hello-welcome">Hello & Welcome, I am</h2>
                {/* Troy Zada, a Student, a Programmer, ... */}
-               <h1 className="home-intro-name">
-                  {FIRST_NAME} {LAST_NAME}!
-               </h1>
+               <h1 className="home-intro-name">{FULL_NAME}!</h1>
                {/* CURRENT ROLE/POSITION */}
                <h4 className="home-intro-program">
                   Management Engineering Student @ University of Waterloo
@@ -31,7 +27,7 @@ function Home() {
             </div>
 
             {/* BRIEF OVERALL DESCRIPTION */}
-            <p className="card-paragraphs">
+            <p id="home-paragraph">
                Motivated and hard-working engineering student with a passion for
                programming, innovation, and optimizing processes. I enjoy
                learning and being able to work out and find solutions to
@@ -39,41 +35,38 @@ function Home() {
                as if you were to live forever." - Mahatma Gandhi
             </p>
 
-            {/* WAYS TO CONTACT */}
-            <div className="home-page-contact">
-               {/* SOCIAL ICON LINKS */}
-               <div className="social-media">
-                  <div className="social-media-container">
-                     {/* ICON FOR FACEBOOK */}
-                     <a
-                        href={FACEBOOK_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="Facebook"
-                     >
-                        <i className="fab fa-facebook-square social-icon"></i>
-                     </a>
-                     {/* ICON FOR EMAIL */}
-                     <a
-                        id="envelope-icon"
-                        href="mailto:tzada@uwaterloo.ca"
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="Email"
-                     >
-                        <p id="email-text">{EMAIL}</p>
-                        <i className="fas fa-envelope-square social-icon"></i>
-                     </a>
-                     {/* ICON FOR LINKEDIN */}
-                     <a
-                        href={LINKEDIN_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="LinkedIn"
-                     >
-                        <i className="fab fa-linkedin social-icon"></i>
-                     </a>
-                  </div>
+            {/* SOCIAL ICON LINKS */}
+            <div className="social-media">
+               <div className="social-media-container">
+                  {/* ICON FOR FACEBOOK */}
+                  <a
+                     href={FACEBOOK_LINK}
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="Facebook"
+                  >
+                     <i className="fab fa-facebook-square social-icon" />
+                  </a>
+                  {/* ICON FOR EMAIL */}
+                  <a
+                     id="envelope-icon"
+                     href="mailto:tzada@uwaterloo.ca"
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="Email"
+                  >
+                     <p id="email-text">{EMAIL}</p>
+                     <i className="fas fa-envelope-square social-icon" />
+                  </a>
+                  {/* ICON FOR LINKEDIN */}
+                  <a
+                     href={LINKEDIN_LINK}
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="LinkedIn"
+                  >
+                     <i className="fab fa-linkedin social-icon" />
+                  </a>
                </div>
             </div>
          </div>
