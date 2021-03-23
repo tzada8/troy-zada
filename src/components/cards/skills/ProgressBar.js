45a551816b18bd96abde9bbe48cbbe6f0740438f
tzada8 <tzada@uwaterloo.ca>
2021-03-23 12:29:01 -0400

    TypeOfSkills not necessary; only using prog. bars

--- src/components/cards/skills/ProgressBar.js
+++ src/components/cards/skills/ProgressBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function ProgressBar() {
    return (
       <div>
-         <h1>Progress Bar</h1>
+         <div className="group-skills-container">
+            {/* GROUP OF TECHNICAL SKILLS */}
+            <a href="/" target="_blank" rel="noreferrer" aria-label="Facebook">
+               <i className="fab fa-facebook-square social-icon" />
+            </a>
+            <h3>Progress Bar</h3>
+         </div>
       </div>
    );
 }
--- src/components/cards/skills/Skills.js
+++ src/components/cards/skills/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TypeOfSkills from "./TypeOfSkills";
+import ProgressBar from "./ProgressBar";
 import "./Skills.css";
 import "../Cards.css";
 import { SKILLS } from "../../Constants";
@@ -16,7 +16,18 @@ function Skills(props) {
 
             {/* REST OF CONTENT */}
             <div className="card-content">
-               <TypeOfSkills />
+               <div className="group-skills-container">
+                  {/* GROUP OF TECHNICAL SKILLS */}
+                  <a
+                     href="/"
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="Facebook"
+                  >
+                     <i className="fab fa-facebook-square social-icon" />
+                  </a>
+                  <h3>Type Of Skills</h3>
+               </div>
             </div>
          </div>
       </div>
