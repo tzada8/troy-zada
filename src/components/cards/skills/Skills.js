1f4c42ff0a80616b17b1f7a5f1176543c583f1d7
tzada8 <tzada@uwaterloo.ca>
2021-03-23 14:02:07 -0400

    temp skills added to Skills.js for format

--- src/components/cards/skills/Skills.js
+++ src/components/cards/skills/Skills.js
@@ -16,20 +16,6 @@ function Skills(props) {
 
             {/* REST OF CONTENT */}
             <div className="card-content">
-               {/* GROUP OF PROFESSIONAL SKILLS */}
-               <div className="group-skills-container">
-                  <h3 className="skills-header">
-                     <i className="fas fa-cogs" />
-                     Professional Skills
-                  </h3>
-                  <hr className="horizontal-bar__skills-title" />
-                  <ProgressBar skillTitle="Example" percent="60%" />
-                  <ProgressBar skillTitle="My Skill" percent="90%" />
-               </div>
-
-               {/* DIVIDER BETWEEN SKILL GROUPS */}
-               <div className="skills-divider" />
-
                {/* GROUP OF INTERPERSONAL SKILLS */}
                <div className="group-skills-container">
                   <h3 className="skills-header">
@@ -37,8 +23,30 @@ function Skills(props) {
                      Interpersonal Skills
                   </h3>
                   <hr className="horizontal-bar__skills-title" />
-                  <ProgressBar skillTitle="Example" percent="60%" />
-                  <ProgressBar skillTitle="My Skill" percent="90%" />
+                  <ProgressBar skillTitle="Communcation" percent="60%" />
+                  <ProgressBar skillTitle="Problem Solving" percent="90%" />
+                  <ProgressBar skillTitle="Time Management" percent="90%" />
+                  <ProgressBar skillTitle="Punctuality" percent="90%" />
+                  <ProgressBar skillTitle="Detail-Oriented" percent="90%" />
+               </div>
+
+               {/* DIVIDER BETWEEN SKILL GROUPS */}
+               <div className="skills-divider" />
+
+               {/* GROUP OF PROFESSIONAL SKILLS */}
+               <div className="group-skills-container">
+                  <h3 className="skills-header">
+                     <i className="fas fa-cogs" />
+                     Professional Skills
+                  </h3>
+                  <hr className="horizontal-bar__skills-title" />
+                  <ProgressBar skillTitle="Java" percent="60%" />
+                  <ProgressBar skillTitle="Python" percent="90%" />
+                  <ProgressBar skillTitle="Excel VBA" percent="90%" />
+                  <ProgressBar skillTitle="HTML" percent="60%" />
+                  <ProgressBar skillTitle="CSS" percent="90%" />
+                  <ProgressBar skillTitle="JavaScript" percent="90%" />
+                  <ProgressBar skillTitle="R" percent="60%" />
                </div>
             </div>
          </div>
