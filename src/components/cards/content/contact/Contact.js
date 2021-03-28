2076299c80b3962303018551516bcd88a158ea01
tzada8 <tzada@uwaterloo.ca>
2021-03-28 15:44:14 -0400

    added emailjs dependancy for contact form

--- src/components/cards/content/contact/Contact.js
+++ src/components/cards/content/contact/Contact.js
@@ -1,23 +1,81 @@
 import React from "react";
+import emailjs from "emailjs-com";
+import Subtitle from "../extra_components/subtitle/Subtitle";
+import GroupsDivider from "../extra_components/groups_divider/GroupsDivider";
+import { EMAIL } from "../../../Constants";
 import "./Contact.css";
 
 function Contact() {
+   function sendEmail(e) {
+      e.preventDefault();
+
+      emailjs
+         .sendForm(
+            "service_gmail",
+            "template_contact_form",
+            e.target,
+            "user_2i1vEGhwrmIRYHtiWXPEf"
+         )
+         .then(
+            (result) => {
+               console.log(result.text);
+            },
+            (error) => {
+               console.log(error.text);
+            }
+         );
+      e.target.reset();
+   }
+
    return (
       <div>
-         <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam
-            aliquam inventore ipsam vel eveniet reprehenderit, voluptates
-            excepturi tempora, accusantium hic, sed quod distinctio sequi?
-            Eveniet dicta incidunt voluptatum quis quasi voluptate at quidem
-            dolor natus architecto sed sequi ut corporis iste minus maxime
-            delectus, mollitia cum neque consectetur ad! Rem cumque praesentium
-            libero reiciendis animi culpa. Cupiditate similique, perferendis,
-            doloribus commodi optio fugiat suscipit tempora, quisquam officiis
-            nihil corrupti! Libero perspiciatis earum totam pariatur,
-            voluptatibus architecto odit molestiae labore qui hic cum culpa odio
-            expedita a sint consequuntur est, fuga accusamus ducimus dolore
-            laborum numquam. Sit incidunt blanditiis quod ea?
-         </p>
+         <Subtitle icon="fas fa-envelope" label="Email" />
+         <p className="email-address">{EMAIL}</p>
+
+         <GroupsDivider />
+
+         <Subtitle icon="fas fa-inbox" label="Send Me a Message" />
+         <div className="contact-me-container">
+            <form id="contact-form" onSubmit={sendEmail}>
+               <input
+                  name="name"
+                  type="text"
+                  className="form-control"
+                  placeholder="Your Name"
+                  required
+               />
+               <br />
+               <input
+                  name="email"
+                  type="email"
+                  className="form-control"
+                  placeholder="Your Email"
+                  required
+               />
+               <br />
+               <input
+                  name="subject"
+                  type="text"
+                  className="form-control"
+                  placeholder="Subject"
+                  required
+               />
+               <br />
+               <textarea
+                  name="message"
+                  className="form-control"
+                  placeholder="Message"
+                  rows="4"
+                  required
+               ></textarea>
+               <br />
+               <input
+                  type="submit"
+                  class="form-control submit"
+                  value="SEND MESSAGE"
+               />
+            </form>
+         </div>
       </div>
    );
 }
