98b95e0f6ac798ecc30d6e40049dec9c5682dfda
tzada8 <tzada@uwaterloo.ca>
2021-03-30 17:32:37 -0400

    removed redundant code

--- src/components/cards/content/portfolio/project_post/ProjectPost.js
+++ src/components/cards/content/portfolio/project_post/ProjectPost.js
@@ -14,16 +14,7 @@ function ProjectPost(props) {
          </figure>
          <div className="project-github-date">
             <h4 className="project-date">{props.date}</h4>
-            <a
-               className="github-link"
-               href={props.github}
-               target="_blank"
-               rel="noreferrer"
-               aria-label="Github"
-            >
-               <i className="fab fa-github" />
-               Github
-            </a>
+            <GithubLink path={props.github} />
          </div>
          <hr className="horizontal-bar__project-posts hr-blog-margin-adjust" />
       </div>
