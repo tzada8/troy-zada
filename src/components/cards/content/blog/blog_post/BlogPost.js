e7934c5945b3215d7e00c1a2b842aeb11329e48e
tzada8 <tzada@uwaterloo.ca>
2021-03-29 23:15:48 -0400

    blog posts have basic header content

--- src/components/cards/content/blog/blog_post/BlogPost.js
+++ src/components/cards/content/blog/blog_post/BlogPost.js
@@ -5,13 +5,17 @@ function BlogPost(props) {
    return (
       <div>
          <figure className="blog-picwrap">
-            <img className="blog-image" src={props.src} alt="Blog Post" />
+            <img
+               className="blog-image post-specific"
+               src={props.src}
+               alt="Blog Post"
+            />
          </figure>
          <div className="blog-description-box">
             <h3 className="blog-title">{props.blogTitle}</h3>
             <h4 className="blog-date">{props.date}</h4>
          </div>
-         <hr className="horizontal-bar__blog-posts" />
+         <hr className="horizontal-bar__blog-posts hr-blog-margin-adjust" />
       </div>
    );
 }
--- src/components/cards/content/blog/subcards/CreatingThisWebsite.js
+++ src/components/cards/content/blog/subcards/CreatingThisWebsite.js
@@ -1,9 +1,16 @@
 import React from "react";
+import BlogPost from "../blog_post/BlogPost";
+import { blogData } from "../BlogData";
 
 function CreatingThisWebsite() {
    return (
       <div>
-         <h1>Creating This Website blog post</h1>
+         <BlogPost
+            src={blogData.creatingThisWebsite.image}
+            blogTitle={blogData.creatingThisWebsite.title}
+            date={blogData.creatingThisWebsite.date}
+         />
+         <p>Creating This Website blog post</p>
       </div>
    );
 }
