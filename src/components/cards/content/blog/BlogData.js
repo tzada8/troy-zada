120fd0c8aac8689cfd2f0690fc4e4e3c59071fd7
tzada8 <tzada@uwaterloo.ca>
2021-04-28 19:52:42 -0400

    added pictures for blog page and fixed home pics

--- src/components/cards/content/blog/BlogData.js
+++ src/components/cards/content/blog/BlogData.js
@@ -1,14 +1,14 @@
-import item1_CreatingThisWebsite from "../../../../images/headshot_1.png";
-import item2_FirstWorkTerm from "../../../../images/headshot_2.png";
+import firstWorkTermImage from "../../../../images/blog/first-work-term.png";
+import creatingThisWebsiteImage from "../../../../images/blog/creating-this-website.jpg";
 
 export const blogData = {
    firstWorkTerm: {
-      image: item2_FirstWorkTerm,
+      image: firstWorkTermImage,
       title: "My First Coop Work Term",
       date: "March 26, 2021",
    },
    creatingThisWebsite: {
-      image: item1_CreatingThisWebsite,
+      image: creatingThisWebsiteImage,
       title: "How I Created This Website",
       date: "March 27, 2021",
    },
