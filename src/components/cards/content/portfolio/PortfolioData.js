7bc089745a3062ee9c4f00166cdd95883f771463
tzada8 <tzada@uwaterloo.ca>
2021-05-01 16:49:13 -0400

    added border to projectCards and 'youtube' to data

--- src/components/cards/content/portfolio/PortfolioData.js
+++ src/components/cards/content/portfolio/PortfolioData.js
@@ -13,43 +13,50 @@ import industry4Image from "../../../../images/portfolio/communication/industry4
 const github = "https://github.com/"; // MAIN LINK TO GITHUB WEBSITE
 const githubMyProfile = github + "tzada8/"; // PATH TO MY PROFILE
 
+const youtube = "https://www.youtube.com/"; // MAIN LINK TO YOUTUBE WEBSITE
+
 export const portfolioData = {
-   troyZadaWebsite: {
-      title: "Troy Zada Website",
-      image: troyZadaImage,
-      date: "TODAY",
-      github: githubMyProfile + "TroyZada",
-   },
    moodivity: {
       title: "Moodivity",
       image: moodivityImage,
       date: "December 8, 2020",
       github: github + "s6eskand/Moodivity",
-      youtube: "www.youtube.com",
+      youtube: youtube,
+   },
+   troyZadaWebsite: {
+      title: "Troy Zada Website",
+      image: troyZadaImage,
+      date: "TODAY",
+      github: githubMyProfile + "TroyZada",
+      youtube: youtube,
    },
    pathfinding: {
       title: "Pathfinding Algorithms",
       image: pathfindingAlgorithmsImage,
       date: "April 27, 2021",
       github: githubMyProfile + "PathfindingAlgorithms",
+      youtube: youtube,
    },
    sudokuSolver: {
       title: "Sudoku Solver",
       image: sudokuSolverImage,
       date: "January 31, 2021",
       github: githubMyProfile + "SudokuSolver",
+      youtube: youtube,
    },
    blackJack: {
       title: "BlackJack",
       image: blackJackImage,
       date: "January 24, 2021",
       github: githubMyProfile + "BlackJack",
+      youtube: youtube,
    },
    dss: {
       title: "Decision Support System",
       image: decisionSupportSystemImage,
       date: "December 16, 2019",
-      github: "www.github.com",
+      github: githubMyProfile + "DecisionSupportSystem",
+      youtube: youtube,
    },
    literatureFair: {
       title: "Literature Fair",
