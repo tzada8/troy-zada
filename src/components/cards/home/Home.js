f142e4a85dde1ce70045f3d60f68b79565766677
tzada8 <tzada@uwaterloo.ca>
2021-05-02 16:27:55 -0400

    hover email icon pop-up working properly

--- src/components/cards/home/Home.js
+++ src/components/cards/home/Home.js
@@ -47,42 +47,40 @@ function Home(props) {
 
             {/* SOCIAL ICON LINKS */}
             <div className="social-media">
-               <div className="social-media-container">
-                  {/* ICON FOR FACEBOOK */}
-                  <div className="icon-frame">
-                     <a
-                        href={FACEBOOK_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="Facebook"
-                     >
-                        <i className="fab fa-facebook-square social-icon" />
-                     </a>
-                  </div>
-                  {/* ICON FOR EMAIL */}
-                  <div className="icon-frame">
-                     <span id="email-text">{EMAIL}</span>
-                     <a
-                        id="envelope-icon"
-                        href={"mailto:" + EMAIL}
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="Email"
-                     >
-                        <i className="fas fa-envelope-square social-icon" />
-                     </a>
-                  </div>
-                  {/* ICON FOR LINKEDIN */}
-                  <div className="icon-frame">
-                     <a
-                        href={LINKEDIN_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                        aria-label="LinkedIn"
-                     >
-                        <i className="fab fa-linkedin social-icon" />
-                     </a>
-                  </div>
+               {/* ICON FOR FACEBOOK */}
+               <div className="icon-frame">
+                  <a
+                     href={FACEBOOK_LINK}
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="Facebook"
+                  >
+                     <i className="fab fa-facebook-square social-icon" />
+                  </a>
+               </div>
+               {/* ICON FOR EMAIL */}
+               <div className="icon-frame">
+                  <a
+                     id="envelope-icon"
+                     href={"mailto:" + EMAIL}
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="Email"
+                  >
+                     <i className="fas fa-envelope-square social-icon" />
+                  </a>
+                  <span id="email-text">{EMAIL}</span>
+               </div>
+               {/* ICON FOR LINKEDIN */}
+               <div className="icon-frame">
+                  <a
+                     href={LINKEDIN_LINK}
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="LinkedIn"
+                  >
+                     <i className="fab fa-linkedin social-icon" />
+                  </a>
                </div>
             </div>
 
