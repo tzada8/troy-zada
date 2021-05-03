343bb37b03c17628592740f7f10edbdd7bef9e10
tzada8 <tzada@uwaterloo.ca>
2021-05-03 18:37:58 -0400

    moved descriptions to constant portfolioData

--- src/components/cards/content/portfolio/Portfolio.js
+++ src/components/cards/content/portfolio/Portfolio.js
@@ -14,37 +14,37 @@ function Portfolio() {
                path="/portfolio/moodivity"
                projectTitle={portfolioData.moodivity.title}
                src={portfolioData.moodivity.image}
-               text="This project was made in November 2020 using the React Framework as well as Django"
+               text={portfolioData.moodivity.briefDescription}
             />
             <ProjectCard
                path="/portfolio/troy-zada"
                projectTitle={portfolioData.troyZadaWebsite.title}
                src={portfolioData.troyZadaWebsite.image}
-               text="This project was made using HTML, CSS, JavaScript, and the React framework in order to make this website"
+               text={portfolioData.troyZadaWebsite.briefDescription}
             />
             <ProjectCard
                path="/portfolio/pathfinding"
                projectTitle={portfolioData.pathfinding.title}
                src={portfolioData.pathfinding.image}
-               text="Used Java including the Swing GUI to create a program that visually compares pathfinding algorithms"
+               text={portfolioData.pathfinding.briefDescription}
             />
             <ProjectCard
                path="/portfolio/sudoku-solver"
                projectTitle={portfolioData.sudokuSolver.title}
                src={portfolioData.sudokuSolver.image}
-               text="This project was made using the Backtracking algorithm and can solve any sudoku board"
+               text={portfolioData.sudokuSolver.briefDescription}
             />
             <ProjectCard
                path="/portfolio/blackjack"
                projectTitle={portfolioData.blackJack.title}
                src={portfolioData.blackJack.image}
-               text="This project was made using Python in addition to Python's GUI framework Tkinter"
+               text={portfolioData.blackJack.briefDescription}
             />
             <ProjectCard
                path="/portfolio/decision-support-system"
                projectTitle={portfolioData.dss.title}
                src={portfolioData.dss.image}
-               text="This project was made using Excel VBA in order to make a project tool that aids user completing group projcets in an efficient manner"
+               text={portfolioData.dss.briefDescription}
             />
          </ul>
 
@@ -57,13 +57,13 @@ function Portfolio() {
                path="/portfolio/literature-fair"
                projectTitle={portfolioData.literatureFair.title}
                src={portfolioData.literatureFair.image}
-               text="This project focuses on the literary theory of existentialism involving how each individual is in charge of their own lives based on their own decisions"
+               text={portfolioData.literatureFair.briefDescription}
             />
             <ProjectCard
                path="/portfolio/industry4"
                projectTitle={portfolioData.industry4.title}
                src={portfolioData.industry4.image}
-               text="This project involved marketing and reaching out to fellow high school about a competition regarding optimization of factory procedures"
+               text={portfolioData.industry4.briefDescription}
             />
          </ul>
       </div>
--- src/components/cards/content/portfolio/PortfolioData.js
+++ src/components/cards/content/portfolio/PortfolioData.js
@@ -19,6 +19,8 @@ export const portfolioData = {
    moodivity: {
       title: "Moodivity",
       image: moodivityImage,
+      briefDescription:
+         "This project was made in November 2020 using the React Framework as well as Django",
       date: "December 8, 2020",
       github: github + "s6eskand/Moodivity",
       youtube: youtube,
@@ -26,6 +28,8 @@ export const portfolioData = {
    troyZadaWebsite: {
       title: "Troy Zada Website",
       image: troyZadaImage,
+      briefDescription:
+         "This project was made using HTML, CSS, JavaScript, and the React framework in order to make this website",
       date: "TODAY",
       github: githubMyProfile + "TroyZada",
       youtube: youtube,
@@ -33,6 +37,8 @@ export const portfolioData = {
    pathfinding: {
       title: "Pathfinding Algorithms",
       image: pathfindingAlgorithmsImage,
+      briefDescription:
+         "Used Java including the Swing GUI to create a program that visually compares pathfinding algorithms",
       date: "April 27, 2021",
       github: githubMyProfile + "PathfindingAlgorithms",
       youtube: youtube,
@@ -40,6 +46,8 @@ export const portfolioData = {
    sudokuSolver: {
       title: "Sudoku Solver",
       image: sudokuSolverImage,
+      briefDescription:
+         "This project was made using the Backtracking algorithm and can solve any sudoku board",
       date: "January 31, 2021",
       github: githubMyProfile + "SudokuSolver",
       youtube: youtube,
@@ -47,6 +55,8 @@ export const portfolioData = {
    blackJack: {
       title: "BlackJack",
       image: blackJackImage,
+      briefDescription:
+         "This project was made using Python in addition to Python's GUI framework Tkinter",
       date: "January 24, 2021",
       github: githubMyProfile + "BlackJack",
       youtube: youtube,
@@ -54,6 +64,8 @@ export const portfolioData = {
    dss: {
       title: "Decision Support System",
       image: decisionSupportSystemImage,
+      briefDescription:
+         "This project was made using Excel VBA in order to make a project tool that aids user completing group projcets in an efficient manner",
       date: "December 16, 2019",
       github: githubMyProfile + "DecisionSupportSystem",
       youtube: youtube,
@@ -61,11 +73,15 @@ export const portfolioData = {
    literatureFair: {
       title: "Literature Fair",
       image: literatureFairImage,
+      briefDescription:
+         "This project focuses on the literary theory of existentialism involving how each individual is in charge of their own lives based on their own decisions",
       date: "June 6, 2019",
    },
    industry4: {
       title: "Industry 4.0",
       image: industry4Image,
+      briefDescription:
+         "This project involved marketing and reaching out to fellow high school about a competition regarding optimization of factory procedures",
       date: "February 17, 2021",
    },
 };
