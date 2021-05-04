0faf4b54f0ec962494b18ac38a1d59e4d203c028
tzada8 <tzada@uwaterloo.ca>
2021-05-04 14:32:03 -0400

    completed Troy Zada Website Portfolio information

--- src/components/cards/content/portfolio/subcards/coding/TroyZada.js
+++ src/components/cards/content/portfolio/subcards/coding/TroyZada.js
@@ -3,6 +3,8 @@ import CodingProjectPost from "../../project_post/coding/CodingProjectPost";
 import { portfolioData } from "../../PortfolioData";
 import Subheading from "../../../extra_components/paragraph/Subheading";
 import Paragraph from "../../../extra_components/paragraph/Paragraph";
+import TypeOfTech from "./tech_stack/TypeOfTech";
+import TechListItem from "./tech_stack/TechListItem";
 
 function TroyZada() {
    return (
@@ -14,8 +16,63 @@ function TroyZada() {
             github={portfolioData.troyZadaWebsite.github}
             youtube={portfolioData.troyZadaWebsite.youtube}
          />
+
          <Subheading subheading="About the Application" />
-         <Paragraph content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae magnam reiciendis, optio adipisci molestiae quo, quam atque eaque distinctio tempore aspernatur culpa voluptate voluptatum repellendus facilis nesciunt doloremque modi cupiditate?" />
+         <Paragraph content="Every individual has their own story to tell, and thus, this application explains my story. Ranging from basic contact information to my past experiences, to skills I possess, to projects I’ve worked on, and lastly, to some shared blog posts. All this explains who Troy Zada is." />
+         <Paragraph content="With this Troy Zada Website, users can view information about me in one of six categories, being Home, Experience, Skills, Portfolio, Blog, and Contact. " />
+         <ul>
+            <TechListItem
+               language="Home"
+               description="Basic information as well as social media links"
+            />
+            <TechListItem
+               language="Experience"
+               description="Education, work experiences, and awards"
+            />
+            <TechListItem
+               language="Skills"
+               description="Percentages for numerous interpersonal and professional skills"
+            />
+            <TechListItem
+               language="Portfolio"
+               description="List of coding projects and communication projects worked on and completed"
+            />
+            <TechListItem
+               language="Blog"
+               description="Stories shared about past experiences"
+            />
+            <TechListItem
+               language="Contact"
+               description="A way to reach out and leave me a message"
+            />
+         </ul>
+         <br />
+
+         <Subheading subheading="Tech Stack" />
+         <TypeOfTech type="Frontend" />
+         <ul>
+            <TechListItem
+               language="React"
+               description="UI framework of the application"
+            />
+            <TechListItem
+               language="JavaScript"
+               description="Frontend language of the application"
+            />
+            <TechListItem
+               language="HTML"
+               description="Markup design of webpage"
+            />
+            <TechListItem
+               language="CSS"
+               description="Styles for elements and components"
+            />
+         </ul>
+         <br />
+
+         <Subheading subheading="Application's Functionality" />
+         <Paragraph content="Each part of the webpage is a unique component, where each component consists of its own set of elements (elements being basic HTML tags such as <div></div>). The components are then rendered to the screen, and the corresponding CSS styles are applied. " />
+         <Paragraph content="The webpage displays a corresponding card depending on what URL the user is currently at. For example, if the URL has “/experience” in it, then the Experience page will be displayed. Similarly, if the URL contains “/portfolio/troy-zada”, then the specific “troy-zada” card from the Portfolio section will be displayed. Home is always rendered." />
       </div>
    );
 }
