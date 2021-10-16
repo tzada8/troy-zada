b023758c75b0772a5eed3d8c6071e8d8d63419ab
tzada8 <tzada@uwaterloo.ca>
2021-10-16 16:31:00 -0400

    moved skills data into a SkillsData.js file and then simplified Skills component by using .map function to loop through skillsData

--- src/components/cards/content/skills/Skills.js
+++ src/components/cards/content/skills/Skills.js
@@ -2,37 +2,29 @@ import React from "react";
 import Subtitle from "../extra-components/subtitle/Subtitle";
 import ProgressBar from "./progress-bar/ProgressBar";
 import GroupsDivider from "../extra-components/groups-divider/GroupsDivider";
+import { skillsData } from "../../../../data/skills/SkillsData";
 
 function Skills() {
+	function displaySkillsData(data) {
+		return (
+			<ul>
+				{data.map((i) => (
+					<ProgressBar label={i.label} percent={i.percent} />
+				))}
+			</ul>
+		);
+	}
+
+	const displayInterpersonalData = displaySkillsData(skillsData.interpersonal);
+	const displayTechnicalData = displaySkillsData(skillsData.technical);
+
 	return (
 		<div>
-			{/* GROUP OF INTERPERSONAL SKILLS */}
 			<Subtitle icon="fas fa-users" label="Interpersonal Skills" />
-			<ul>
-				<ProgressBar label="Communcation" percent="96%" />
-				<ProgressBar label="Problem Solving" percent="92%" />
-				<ProgressBar label="Willingness to Learn" percent="100%" />
-				<ProgressBar label="Time Management" percent="89%" />
-				<ProgressBar label="Punctuality" percent="88%" />
-				<ProgressBar label="Detail-Oriented" percent="98%" />
-				<ProgressBar label="Cross-Functional" percent="86%" />
-				<ProgressBar label="Teamwork" percent="90%" />
-			</ul>
-
+			{displayInterpersonalData}
 			<GroupsDivider />
-
-			{/* GROUP OF PROFESSIONAL SKILLS */}
-			<Subtitle icon="fas fa-cogs" label="Professional Skills" />
-			<ul>
-				<ProgressBar label="Java" percent="82%" />
-				<ProgressBar label="Python" percent="74%" />
-				<ProgressBar label="HTML" percent="84%" />
-				<ProgressBar label="CSS" percent="80%" />
-				<ProgressBar label="Excel VBA" percent="58%" />
-				<ProgressBar label="React" percent="78%" />
-				<ProgressBar label="JavaScript" percent="68%" />
-				<ProgressBar label="R" percent="48%" />
-			</ul>
+			<Subtitle icon="fas fa-cogs" label="Technical Skills" />
+			{displayTechnicalData}
 		</div>
 	);
 }
--- src/components/cards/content/skills/progress-bar/ProgressBar.js
+++ src/components/cards/content/skills/progress-bar/ProgressBar.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./ProgressBar.css";
 
 function ProgressBar(props) {
+	const numberAsPercent = props.percent + "%";
+
 	return (
 		<li className="progress-container">
 			<h4 className="progress-header">
 				{props.label}
-				<span className="percentage">{props.percent}</span>
+				<span className="percentage">{numberAsPercent}</span>
 			</h4>
 			<div className="progress-bar__border">
 				<div
 					className="progress-bar__progress"
-					style={{ width: props.percent }}
+					style={{ width: numberAsPercent }}
 				/>
 			</div>
 		</li>
