7b1baa2e1919d645221d6e0a26cb4c6c6c5af47c
Troy Zada <tzada@uwaterloo.ca>
2024-07-08 10:44:01 -0400

    Updated headshot images

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,35 +14,37 @@ export default function Home(props) {
 	return (
 		<div className={`card-box center ${props.isOnlyComp ? "home-center" : "home-left"}`}>
 			<div className="card-container">
-				<div className={`home-img-round ${props.isOnlyComp ? "headshot1" : "headshot2"}`} />
+				<div className="card-content center">
+					<div className={`home-img-round ${props.isOnlyComp ? "headshot1" : "headshot2"}`} />
 
-				<div className="home-intro-title">
-					{/* Troy Zada, an Engineering Student, a Programmer, Lifelong Learner, ... */}
-					<h1 className="home-intro-name">{FULL_NAME}</h1>
-					<h3 className="home-intro-program">
-						Management Engineering Student @ University of Waterloo
-					</h3>
-					<HorizontalBar gradient />
+					<div className="home-intro-title">
+						{/* Troy Zada, an Engineering Student, a Programmer, Lifelong Learner, ... */}
+						<h1 className="home-intro-name">{FULL_NAME}</h1>
+						<h3 className="home-intro-program">
+							Management Engineering Student @ University of Waterloo
+						</h3>
+						<HorizontalBar gradient />
+					</div>
+
+					<p>
+						Dedicated and hard-working engineering student with a passion for
+						programming, innovation, optimization, and learning. "Live as if
+						you were to die tomorrow. Learn as if you were to live forever."
+						- M.K. Gandhi
+					</p>
+
+					<div className="social-media">
+						{socials.map((s, k) => (
+							<SocialIcon key={k} link={s.link} icon={s.icon} tooltip={s.tooltip} />
+						))}
+					</div>
+
+					<RoutingButton
+						path={routes[contact].path}
+						label="GET IN TOUCH"
+						icon={<MdContactMail />}
+					/>
 				</div>
-
-				<p>
-					Dedicated and hard-working engineering student with a passion for
-					programming, innovation, optimization, and learning. "Live as if
-					you were to die tomorrow. Learn as if you were to live forever."
-					- M.K. Gandhi
-				</p>
-
-				<div className="social-media">
-					{socials.map((s, k) => (
-						<SocialIcon key={k} link={s.link} icon={s.icon} tooltip={s.tooltip} />
-					))}
-				</div>
-
-				<RoutingButton
-					path={routes[contact].path}
-					label="GET IN TOUCH"
-					icon={<MdContactMail />}
-				/>
 			</div>
 		</div>
 	);
