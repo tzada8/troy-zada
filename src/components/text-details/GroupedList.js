bff516bc14bc2b9f10168ce9f0f482af12a349ec
Troy Zada <tzada@uwaterloo.ca>
2024-07-12 18:06:02 -0400

    Added Juxtapose info

--- src/components/text-details/GroupedList.js
+++ src/components/text-details/GroupedList.js
@@ -5,7 +5,7 @@ import "./TextDetails.css";
 export default function GroupedList(props) {
     return (
         <div>
-            <p className="bold justify">{props.header}</p>
+            <p className={`${props.header.includes(":") ? "": "bold"} justify`}>{props.header}</p>
             <ul>
                 {props.bullets.map((b, k) => <li key={k} className="list-item">
                     <p className="justify"><span className="bold">{b.label}:</span> {b.content}</p>
--- src/pages/portfolio/projects/Juxtapose.js
+++ src/pages/portfolio/projects/Juxtapose.js
@@ -1,13 +1,51 @@
 import React from "react";
 
+import GroupedList from "../../../components/text-details/GroupedList";
 import Paragraph from "../../../components/text-details/Paragraph";
 import Subheading from "../../../components/text-details/Subheading";
 
 export default function Juxtapose() {
 	return (
 		<div>
-			<Subheading label="About the Application" />
-			<Paragraph content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae magnam reiciendis, optio adipisci molestiae quo, quam atque eaque distinctio tempore aspernatur culpa voluptate voluptatum repellendus facilis nesciunt doloremque modi cupiditate?" />
+			<Subheading first label="About the Application" />
+			<Paragraph content="Juxtapose is a web application designed to help online shoppers tackle the difficulty of making well-informed and confident purchasing decisions by alleviating decision paralysis and information overload." />
+			<Paragraph content="By aggregating specifications, peer reviews, expert reviews, and related videos, and providing tailored recommendations, shoppers can reach purchasing decisions with ease." />
+			<GroupedList header="To use Juxtapose, users can:" bullets={[
+				{label: "Search", content: "Search for product category (ex. DSLR cameras)"},
+				{label: "Set Filters & Preferences", content: "Set filters and rank features based on personal preferences"},
+				{label: "View Recommendations", content: "See the products recommended to them"},
+				{label: "Compare", content: "Compare products and pick their favourite one"},
+			]}/>
+
+			<Subheading label="Tech Stack" />
+			<GroupedList header="Frontend" bullets={[
+				{label: "React", content: "UI framework"},
+				{label: "JavaScript", content: "Frontend language"},
+				{label: "Ant Design", content: "Predesigned icons"},
+				{label: "Google Fonts", content: "Custom font styles"},
+			]}/>
+
+			<GroupedList header="Backend" bullets={[
+				{label: "Flask", content: "Backend framework"},
+				{label: "Python", content: "Backend language"},
+			]}/>
+
+			<GroupedList header="APIs" bullets={[
+				{label: "Best Buy API", content: "Scrape product information from Best Buy"},
+				{label: "OpenAI", content: "Summarize product reviews"},
+				{label: "RapidAPI", content: "Scrape expert reviews from Consumer Reports"},
+				{label: "SerpAPI", content: "Scrape products from Google Shopping, product information from Walmart, and related product YouTube videos"},
+			]}/>
+
+			<GroupedList header="Deployment" bullets={[
+				{label: "Render", content: "Frontend deployment"},
+				{label: "Heroku", content: "Backend deployment"},
+			]}/>
+
+			<Subheading label="Technical Functionality" />
+			<Paragraph content="A user searches for products such as a “DSLR Camera”. From their search, they provide preferences such as their preferred budget, camera type, and quality, and relevant products are pulled from Google Shopping and displayed to the user." />
+			<Paragraph content="The user can then select the products they are interested in, and then are prompted to rank their preferences. For instance, one user may consider budget to be the most important, but another user may not want to factor budget into their decision-making process." />
+			<Paragraph last content="After the preferences are prioritized, all relevant product information for the selected products is pulled from Best Buy and Walmart. The specifications and peer reviews are pulled directly from Best Buy and Walmart, the expert reviews are pulled from Consumer Reports, and related videos are pulled from YouTube. Users can then compare the products in a side-by-side view to determine which product is most suitable for them." />
 		</div>
 	);
 }
