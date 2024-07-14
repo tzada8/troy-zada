641bb3afea9fd68c3a727f80d45d38d6a263f118
Troy Zada <tzada@uwaterloo.ca>
2024-07-14 01:01:56 -0400

    Removed incomplete creating website blog post

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -1,12 +1,10 @@
 import { rootRoutes } from "../routes/root-routes";
 
 // Images
-import creatingThisWebsiteImage from "../images/blog/creating-this-website.webp";
 import firstWorkTermImage from "../images/blog/first-work-term.webp";
 import universityLifeLessonsImage from "../images/blog/university-life-lessons.webp";
 
 // Components
-import CreatingThisWebsite from "../pages/blog/posts/CreatingThisWebsite";
 import FirstWorkTerm from "../pages/blog/posts/FirstWorkTerm";
 import UniversityLifeLessons from "../pages/blog/posts/UniversityLifeLessons";
 
@@ -24,12 +22,5 @@ export const blogData = [
 		element: <FirstWorkTerm />,
 		image: firstWorkTermImage,
 		date: "June 1, 2024",
-    },
-    {
-        label: "How I Created This Website",
-        path: `${rootRoutes.blog.path}/creating-this-website`,
-		element: <CreatingThisWebsite />,
-		image: creatingThisWebsiteImage,
-		date: "March 27, 2021", // TODO: Update.
     },
 ]
