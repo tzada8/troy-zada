58c8b6e01252309df0869af48fc74bda63d88c39
Troy Zada <tzada@uwaterloo.ca>
2024-07-14 00:52:22 -0400

    Added new blog post

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -3,18 +3,27 @@ import { rootRoutes } from "../routes/root-routes";
 // Images
 import creatingThisWebsiteImage from "../images/blog/creating-this-website.webp";
 import firstWorkTermImage from "../images/blog/first-work-term.webp";
+import universityLifeLessonsImage from "../images/blog/university-life-lessons.webp";
 
 // Components
 import CreatingThisWebsite from "../pages/blog/posts/CreatingThisWebsite";
 import FirstWorkTerm from "../pages/blog/posts/FirstWorkTerm";
+import UniversityLifeLessons from "../pages/blog/posts/UniversityLifeLessons";
 
 export const blogData = [
     {
-        label: "My First Co-op Work Term",
+        label: "10 Major University Life Lessons",
+        path: `${rootRoutes.blog.path}/university-life-lessons`,
+		element: <UniversityLifeLessons />,
+		image: universityLifeLessonsImage,
+		date: "July 1, 2024",
+    },
+    {
+        label: "First Co-op Work Term",
         path: `${rootRoutes.blog.path}/first-work-term`,
 		element: <FirstWorkTerm />,
 		image: firstWorkTermImage,
-		date: "July 1, 2024",
+		date: "June 1, 2024",
     },
     {
         label: "How I Created This Website",
---
+++ src/pages/blog/posts/UniversityLifeLessons.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Paragraph from "../../../components/text-details/Paragraph";
+import Subheading from "../../../components/text-details/Subheading";
+
+export default function UniversityLifeLessons() {
+	return (
+		<div>
+			<Paragraph content="During my time at university, I gained valuable insights that have been beneficial both academically and in everyday life. In this post, I share 10 key lessons from my university experience that might also enhance your daily life. These lessons are presented in no specific order." />
+
+			<Subheading label="1. Use the Teaching Assistants (TAs)" />
+			<Paragraph content="TAs have always been available to help me succeed and understand the course material. Whenever I was confused about a concept, the teaching team was always an email away to provide clarification; 99 times out of 100 they are more than happy to help." />
+
+			<Subheading label="2. Save Time for Social Life" />
+			<Paragraph content="Doing schoolwork constantly is not beneficial for either mental health or academics. Social breaks throughout the day are essential. I typically give myself 3-4 hours each day where I don’t do any schoolwork at all. During this time, I engage in activities I enjoy such as reading or spending time with friends. Taking breaks from schoolwork helps my mind relax, making me more effective when I return to my studies." />
+
+			<Subheading label="3. The Importance of Group Members" />
+			<Paragraph content="Group projects are a frequent occurrence in university. At times, I had the opportunity to select my own group members, while other times groups were assigned. In both cases, the members of the group are crucial. Effective groups collaborate well and are highly productive, whereas unproductive groups tend to struggle. Therefore, choosing team members who contribute equally can save considerable time for everyone involved." />
+
+			<Subheading label="4. Live Near Campus Over Commuting" />
+			<Paragraph content="Given the option to commute to campus or live near campus, I would opt to live near campus without hesitation. Living nearby significantly saves time that would otherwise be spent commuting, allowing more time for schoolwork or social activities." />
+
+			<Subheading label="5. Learn to Cook" />
+			<Paragraph content="Cooking does demand more time compared to buying food every day, but I’ve found it incredibly beneficial. Even basic cooking skills can save money and offer a productive break from schoolwork." />
+
+			<Subheading label="6. Networking" />
+			<Paragraph content="In short, networking is life changing. It can uncover career opportunities that previously seemed out of reach, purely based on your connections." />
+
+			<Subheading label="7. Use a Calendar and To-do List" />
+			<Paragraph content="Effectively managing time is crucial. At university, I often faced multiple assessments due th same time. To handle this, I immediately noted deadlines as soon as I was aware of them. This helped me visualize my tasks and their deadlines. My to-do list compiled the tasks from different courses in one place, allowing me to prioritize them easily. Similarly, my calendar helped me plan my schedule, assessment work, and free time." />
+
+			<Subheading label="8. Prioritization" />
+			<Paragraph content="Organizing the order work needs to be completed is highly valuable. For instance, working on an assignment that’s due in three weeks when there’s another due tomorrow is generally inefficient. Instead, organizing tasks based on their urgency and complexity is more effective. Personally, I find it beneficial to prioritize tasks first by their deadlines and then by complexity. If two tasks are due on the same day, I assess the time required to complete each task and start with the easier one. This approach offers several advantages: 1) it provides an immediate sense of accomplishment motivating me to complete the complex task, 2) leaves ample time to complete the complex task, and 3) helps get me into a work mindset for the complex task. Instead, if the more complex task was prioritized first, this would just result in two incomplete tasks and increased stress." />
+
+			<Subheading label="9. Study to “Learn” and Study for the “Test”" />
+			<Paragraph content="People typically attend university for two primary reasons: to learn and to earn a degree. To succeed in both aspects, I’ve found it necessary to adapt my mindset accordingly. The mindset for genuine learning and expanding knowledge involves analyzing content by making connections to existing concepts and simplifying complex problems to familiar ones. Conversely, the mindset for performing well on assessments requires analyzing content to anticipate exam questions and preparing answers accordingly. By approaching new material with this dual perspective, I’ve been able to grasp and retain new knowledge effectively while achieving good grades on assessments." />
+
+			<Subheading label="10. Explore" />
+			<Paragraph last content="Aside from obtaining a degree, the primary goal of university is to learn and explore your passions. Use the time to experiment and discover what truly interests you. Once you begin full-time work after graduation, making changes becomes much more challenging." />
+		</div>
+	);
+}
