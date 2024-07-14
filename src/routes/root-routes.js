dc734e1e0ba71bdfe7bd5e42d533de53b717cb77
Troy Zada <tzada@uwaterloo.ca>
2024-07-14 01:23:50 -0400

    Applied linting styles and routes

--- src/routes/root-routes.js
+++ src/routes/root-routes.js
@@ -1,9 +1,9 @@
 export const rootRoutes = {
-    home: {label: "HOME", path: "/"},
-    education: {label: "EDUCATION", path: "/education"},
-    experience: {label: "EXPERIENCE", path: "/experience"},
-    skills: {label: "SKILLS", path: "/skills"},
-    portfolio: {label: "PORTFOLIO", path: "/portfolio"},
-    blog: {label: "BLOG", path: "/blog"},
-    contact: {label: "CONTACT", path: "/contact"},
+	home: { label: "HOME", path: "/" },
+	education: { label: "EDUCATION", path: "/education" },
+	experience: { label: "EXPERIENCE", path: "/experience" },
+	skills: { label: "SKILLS", path: "/skills" },
+	portfolio: { label: "PORTFOLIO", path: "/portfolio" },
+	blog: { label: "BLOG", path: "/blog" },
+	contact: { label: "CONTACT", path: "/contact" },
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,61 +12,62 @@ import Portfolio from "../pages/portfolio/Portfolio";
 import Skills from "../pages/skills/Skills";
 
 export const home = {
-    ...rootRoutes.home,
-    label: FULL_NAME,
-    sublabel: "Management Sciences PhD Student @ University of Waterloo",
-    description: "Dedicated and diligent engineering graduate passionate about learning and teaching. Currently focused on researching how programming and optimization can enhance search engines and recommendation systems.",
+	...rootRoutes.home,
+	label: FULL_NAME,
+	sublabel: "Management Sciences PhD Student @ University of Waterloo",
+	description:
+		"Dedicated and diligent engineering graduate passionate about learning and teaching. Currently focused on researching how programming and optimization can enhance search engines and recommendation systems.",
 };
 
 export const pageNotFound = {
-    label: "PAGE NOT FOUND",
-    path: "*",
+	label: "PAGE NOT FOUND",
+	path: "*",
 	element: <PageNotFound />,
 	quote: "Understanding a question is half the answer",
 	author: "Socrates",
 };
 
 export const routes = [
-    {
-        ...rootRoutes.education,
-        element: <Education />,
+	{
+		...rootRoutes.education,
+		element: <Education />,
 		quote: "Live as if you were to die tomorrow. Learn as if you were to live forever",
 		author: "M.K. Gandhi",
-    },
-    {
-        ...rootRoutes.experience,
-        element: <Experience />,
+	},
+	{
+		...rootRoutes.experience,
+		element: <Experience />,
 		quote: "The only source of knowledge is experience",
 		author: "Albert Einstein",
-    },
-    {
-        ...rootRoutes.skills,
-        element: <Skills />,
+	},
+	{
+		...rootRoutes.skills,
+		element: <Skills />,
 		quote: "Tell me and I forget, teach me and I may remember, involve me and I learn",
 		author: "Benjamin Franklin",
-    },
-    {
-        ...rootRoutes.portfolio,
-        element: <Portfolio />,
+	},
+	{
+		...rootRoutes.portfolio,
+		element: <Portfolio />,
 		quote: "As practice makes perfect, I cannot but make progress; each drawing one makes, each study one paints, is a step forward",
 		author: "Vincent Van Gogh",
-        subroutes: portfolioData,
-    },
-    {
-        ...rootRoutes.blog,
-        element: <Blog />,
+		subroutes: portfolioData,
+	},
+	{
+		...rootRoutes.blog,
+		element: <Blog />,
 		quote: "The reading of all good books is like a conversation with the finest minds of past centuries",
 		author: "Rene Descartes",
-        subroutes: blogData,
-    },
-    {
-        ...rootRoutes.contact,
-        element: <Contact />,
+		subroutes: blogData,
+	},
+	{
+		...rootRoutes.contact,
+		element: <Contact />,
 		quote: "The purpose of life is to live it, to taste experience to the utmost, to reach out eagerly and without fear for newer and richer experience",
 		author: "Eleanor Roosevelt",
-    },
+	},
 ];
 
-export const portfolio = routes.findIndex(route => route.path === rootRoutes.portfolio.path);
-export const blog = routes.findIndex(route => route.path === rootRoutes.blog.path);
-export const contact = routes.findIndex(route => route.path === rootRoutes.contact.path);
+export const portfolio = routes.findIndex((route) => route.path === rootRoutes.portfolio.path);
+export const blog = routes.findIndex((route) => route.path === rootRoutes.blog.path);
+export const contact = routes.findIndex((route) => route.path === rootRoutes.contact.path);
