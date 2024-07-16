83d3f3ffd50f6a0038d6272ad32ae4b022e85fcc
Troy Zada <tzada@uwaterloo.ca>
2024-07-16 13:08:25 -0400

    Update date abbreviations

--- src/data/education-data.js
+++ src/data/education-data.js
@@ -9,7 +9,7 @@ export const educationData = [
 			{
 				label: "Management Sciences, PhD",
 				company: "University of Waterloo",
-				duration: "Sept. 2024 - Present",
+				duration: "Sep 2024 - Present",
 				details: [
 					"Research interests oriented towards search engines and recommendation systems.",
 				],
@@ -17,7 +17,7 @@ export const educationData = [
 			{
 				label: "Management Engineering, BASc",
 				company: "University of Waterloo",
-				duration: "Sept. 2019 - June 2024",
+				duration: "Sep 2019 - Jun 2024",
 				details: [
 					"Expertise in data analytics, information systems, and operations management.",
 					"Relevant courses: Machine Learning, Search Engines, Software Engineering, and Optimization.",
@@ -32,28 +32,28 @@ export const educationData = [
 		data: [
 			{
 				label: "Super.com Transformative Impact",
-				duration: "Apr. 2023, Dec. 2023",
+				duration: "Apr 2023, Dec 2023",
 				details: [
 					"Awarded twice consecutively among 60 interns for outstanding performance and delivering significant impact.",
 				],
 			},
 			{
 				label: "Gonzaga Leadership Award",
-				duration: "June 2019",
+				duration: "Jun 2019",
 				details: [
 					"Awarded out of 500 students for being an effective, responsible, and confident problem solver within the school community.",
 				],
 			},
 			{
 				label: "Michael Kim Award",
-				duration: "June 2019",
+				duration: "Jun 2019",
 				details: [
 					"Awarded out of 400 student-athletes for outstanding contributions and commitment to the athletic community.",
 				],
 			},
 			{
 				label: "Knights of Colombus - St. Faustina Award",
-				duration: "June 2019",
+				duration: "Jun 2019",
 				details: [
 					"Awarded out of 500 students for consistently demonstrating an exemplary ethical attitude towards fellow students.",
 				],
