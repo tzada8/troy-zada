3d2600aee6ae54bd066edf900d3cc64d75441a38
Troy Zada <tzada@uwaterloo.ca>
2024-07-17 16:21:04 -0400

    Fixed formatting and linting

--- tests/components/skills-block/SkillsBlock.test.js
+++ tests/components/skills-block/SkillsBlock.test.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import SkillsBlock from '../../../src/components/skills-block/SkillsBlock';
+import SkillsBlock from "../../../src/components/skills-block/SkillsBlock";
 
-describe('SkillsBlock Component', () => {
-    const testData = [
-        { label: 'JavaScript', image: 'javascript.png' },
-        { label: 'React', image: 'react.png' },
-        { label: 'CSS', image: 'css.png' },
-    ];
+describe("SkillsBlock Component", () => {
+	const testData = [
+		{ label: "JavaScript", image: "javascript.png" },
+		{ label: "React", image: "react.png" },
+		{ label: "CSS", image: "css.png" },
+	];
 
-    it('renders skills correctly', () => {
-        const { getByText, getAllByAltText } = render(<SkillsBlock data={testData} />);
+	it("renders skills correctly", () => {
+		render(<SkillsBlock data={testData} />);
 
-        testData.forEach(skill => {
-            const skillElement = getByText(skill.label);
-            expect(skillElement).toBeInTheDocument();
-        });
+		testData.forEach((skill) => {
+			const skillElement = screen.getByText(skill.label);
+			expect(skillElement).toBeInTheDocument();
+		});
 
-        const skillImages = getAllByAltText(/.*/);
-        skillImages.forEach((img, index) => {
-            expect(img).toHaveAttribute('src', testData[index].image);
-            expect(img).toHaveAttribute('alt', testData[index].label);
-        });
-    });
+		const skillImages = screen.getAllByAltText(/.*/);
+		skillImages.forEach((img, index) => {
+			expect(img).toHaveAttribute("src", testData[index].image);
+			expect(img).toHaveAttribute("alt", testData[index].label);
+		});
+	});
 
-    it('renders correct number of skills', () => {
-        const { container } = render(<SkillsBlock data={testData} />);
-        const skillContainers = container.getElementsByClassName('skill-container');
-        expect(skillContainers.length).toBe(testData.length);
-    });
+	it("renders correct number of skills", () => {
+		render(<SkillsBlock data={testData} />);
+		const skillContainers = screen.getAllByTestId("skill-container");
+		expect(skillContainers.length).toBe(testData.length);
+	});
 });
